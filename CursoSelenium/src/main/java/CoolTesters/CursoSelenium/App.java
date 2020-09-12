1eb91684a549b67f02a858e8a46f671ddab4d33e
Victor <Victor@VictorDell>
2020-09-12 10:17:22 -0500

    Initial Commit

---
+++ CursoSelenium/src/main/java/CoolTesters/CursoSelenium/App.java
@@ -0,0 +1,23 @@
+package CoolTesters.CursoSelenium;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "driver" + File.separator;//Tomando path driver en el sistema
+        System.setProperty("webdriver.chrome.driver",  exePath + "chromedriver.exe");//Establecer la propiedad del .exe
+        WebDriver driver = new ChromeDriver();//Inicializando el driver
+        driver.get("https://www.record.com.mx/");//Abrir url
+    }
+}
